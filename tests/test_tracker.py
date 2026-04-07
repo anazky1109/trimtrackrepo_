f8b54ca67c7696d8dd2cf48f014ff9605e6868de
intanesz8 <fancyguv@gmail.com>
2026-04-07 20:19:49 +0700

    mengubah code test karena logika codenya berubah

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,20 +1,30 @@
 import pytest
-from app.tracker import TrimTrack
+import os
+from app.tracker import BarberTracker
 
 @pytest.fixture
-def tracker():
-    return TrimTrack()
+def temp_tracker():
+    """Setup tracker dengan file JSON sementara khusus testing"""
+    test_file = "test_db.json"
+    tracker = BarberTracker(filename=test_file)
+    yield tracker
+    if os.path.exists(test_file):
+        os.remove(test_file)
 
-def test_add_log_success(tracker):
-    res = tracker.add_log("Lari Pagi", 30)
-    assert res["activity"] == "Lari Pagi"
-    assert len(tracker.records) == 1
+def test_reza_validation(temp_tracker):
+    # Tes Anti-Collision Reza
+    temp_tracker.add_booking("Anazky", 10)
+    result = temp_tracker.add_booking("Dyvta", 10)
+    assert "FAILED" in result
 
-def test_invalid_activity(tracker):
-    with pytest.raises(ValueError, match="Nama aktivitas harus diisi"):
-        tracker.add_log("", 10)
+def test_dyvta_storage(temp_tracker):
+    # Tes JSON Storage Dyvta
+    temp_tracker.add_booking("Reza", 14)
+    assert os.path.exists("test_db.json")
 
-def test_total_calculation(tracker):
-    tracker.add_log("Coding", 60)
-    tracker.add_log("Meeting", 30)
-    assert tracker.get_total_duration() == 90
+def test_talitha_report(temp_tracker):
+    # Tes Standarisasi Talitha
+    temp_tracker.add_booking("Z", 15)
+    temp_tracker.add_booking("A", 9)
+    report = temp_tracker.get_schedule_report()
+    assert "[9:00] - A" in report # Pastikan A (jam 9) muncul duluan
